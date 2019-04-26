8791d03a0f6c4857efd8edbb8ee3eae74fe087a6
Selbstdenker <34884262+Selbstdenker@users.noreply.github.com>
2019-04-26 05:04:58 -0300

    0.2a
    adição de um filtro de strings e correção sintática

--- js/pushData.js
+++ js/pushData.js
@@ -1,24 +1,33 @@
-var myArray = [];
 function pushData(){ 
+    var myArray = [];
     var inputText = document.getElementById('inputText').value;
+
     if(inputText == null || inputText == '' || inputText == ' '){
         alert("Este campo não pode ficar vazio");
         return false;
     }
+
     var regex = /,/gi; 
     var remove = inputText.replace(regex, ''); 
+    
     var lower = remove.toLowerCase(); 
     var str = lower.split(' ');
-    var strLength = remove.split(' ').length;
-    var myArray = str; 
+
+    var strCLS = str.filter(String);
+
+    var strLength = strCLS.length;
+    alert(strCLS); alert(strLength);
+
+    var myArray = strCLS; 
     var myArrayMirror = myArray;
-    for (let index = 0; index < strLength; index++) {
-        const elementA = myArray[index]; 
+
+    for(let index = 0; index < strLength; index++) {
+        var elementA = myArray[index]; 
         var table = document.getElementById('output'); 
         var row = table.insertRow(index);
         let i = 0; 
         while(i < strLength) {
-            const elementB = myArrayMirror[i];
+            var elementB = myArrayMirror[i];
             
             if(elementA == elementB){ 
                 var cell = row.insertCell(i);
@@ -32,10 +41,10 @@ function pushData(){
         }
     }
 }
-function clearTextArea() {
+function clearTextArea(){
     document.getElementById('inputText').value = '';
 }
-function clearTable() {
+function clearTable(){
     var rowPull = document.getElementById('output');
     while(rowPull.hasChildNodes())
     {
