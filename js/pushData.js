b343adbc2619a847f2616a7dbf3936f4cf8c9fa4
Selbstdenker <34884262+Selbstdenker@users.noreply.github.com>
2019-04-28 20:53:50 -0300

    refactorin

--- js/pushData.js
+++ js/pushData.js
@@ -1,53 +1,7 @@
-function pushData(){ 
-    var myArray = [];
-    var inputText = document.getElementById('inputText').value;
-
-    if(inputText == null || inputText == '' || inputText == ' '){
-        alert("Este campo não pode ficar vazio");
-        return false;
-    }
-
-    var regex = /,/gi; 
-    var remove = inputText.replace(regex, ''); 
-    
-    var lower = remove.toLowerCase(); 
-    var str = lower.split(' ');
-
-    var strCLS = str.filter(String);
-
-    var strLength = strCLS.length;
-    alert(strCLS); alert(strLength);
-
-    var myArray = strCLS; 
+var pushData = (textareaID, tableID) => { 
+    var inputText = document.getElementById(textareaID).value;
+    nullNot(inputText); 
+    var myArray = strFormat(inputText); 
     var myArrayMirror = myArray;
-
-    for(let index = 0; index < strLength; index++) {
-        var elementA = myArray[index]; 
-        var table = document.getElementById('output'); 
-        var row = table.insertRow(index);
-        let i = 0; 
-        while(i < strLength) {
-            var elementB = myArrayMirror[i];
-            
-            if(elementA == elementB){ 
-                var cell = row.insertCell(i);
-                cell.innerHTML = "▓▓";
-            }
-            else{
-                var cell = row.insertCell(i);
-                cell.innerHTML = "░░";
-            } 
-            i++; 
-        }
-    }
-}
-function clearTextArea(){
-    document.getElementById('inputText').value = '';
-}
-function clearTable(){
-    var rowPull = document.getElementById('output');
-    while(rowPull.hasChildNodes())
-    {
-        rowPull.removeChild(rowPull.firstChild);
-    }
+    selfSim(myArray, myArrayMirror, tableID); 
 }
