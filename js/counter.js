36612589d1d994533028c97b7735e6505dc5f4cb
Selbstdenker <34884262+Selbstdenker@users.noreply.github.com>
2019-05-06 10:02:50 -0300

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,16 +1,13 @@
 var count = (ArrayA, ArrayB, MatchArray, btnID) => {
     var matchArray = [...new Set(MatchArray)];
-    var a, b, c = 0
+    var a, b = 0
     a = ArrayA.length
     b = ArrayB.length
-    c = matchArray.length
     if(btnID == 'AA' || btnID == 'BB'){
         document.getElementById('wordsA').innerHTML = 'Total Words: ' + a
         document.getElementById('wordsB').innerHTML = 'Unique Words: ' + c
-        document.getElementById('wordsC').innerHTML = 'Selfsim Words: ' + (a - c)
     }else{
         document.getElementById('wordsA').innerHTML = 'Total Words: ' + (a + b)
         document.getElementById('wordsB').innerHTML = 'Unique Words: ' + c
-        document.getElementById('wordsC').innerHTML = 'Selfsim Words: ' + (a + b - c)
     }     
 }
