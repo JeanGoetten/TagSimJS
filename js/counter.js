be61882232953fba5e1a4fee67e9d3a40f7570a4
Selbstdenker <34884262+Selbstdenker@users.noreply.github.com>
2019-05-06 10:00:16 -0300

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,12 +1,16 @@
 var count = (ArrayA, ArrayB, MatchArray, btnID) => {
     var matchArray = [...new Set(MatchArray)];
+    var a, b, c = 0
+    a = ArrayA.length
+    b = ArrayB.length
+    c = matchArray.length
     if(btnID == 'AA' || btnID == 'BB'){
-        document.getElementById('wordsA').innerHTML = 'Total Words: ' + ArrayA.length
-        document.getElementById('wordsB').innerHTML = 'Unique Words: ' + matchArray.length
-        document.getElementById('wordsC').innerHTML = 'Selfsim Words: ' + (ArrayA.length - matchArray.length)
+        document.getElementById('wordsA').innerHTML = 'Total Words: ' + a
+        document.getElementById('wordsB').innerHTML = 'Unique Words: ' + c
+        document.getElementById('wordsC').innerHTML = 'Selfsim Words: ' + (a - c)
     }else{
-        document.getElementById('wordsA').innerHTML = 'Total Words: ' + (ArrayA.length + ArrayB.length)
-        document.getElementById('wordsB').innerHTML = 'Unique Words: ' + matchArray.length
-        document.getElementById('wordsC').innerHTML = 'Selfsim Words: ' + (ArrayA.length + ArrayB.length - matchArray.length)
+        document.getElementById('wordsA').innerHTML = 'Total Words: ' + (a + b)
+        document.getElementById('wordsB').innerHTML = 'Unique Words: ' + c
+        document.getElementById('wordsC').innerHTML = 'Selfsim Words: ' + (a + b - c)
     }     
 }
