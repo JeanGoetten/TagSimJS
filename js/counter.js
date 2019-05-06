610082f544d09a339cdc4843f13c4a187ee46d8a
Selbstdenker <34884262+Selbstdenker@users.noreply.github.com>
2019-05-06 09:55:21 -0300

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -3,10 +3,10 @@ var count = (ArrayA, ArrayB, MatchArray, btnID) => {
     if(btnID == 'AA' || btnID == 'BB'){
         document.getElementById('wordsA').innerHTML = 'Total Words: ' + ArrayA.length
         document.getElementById('wordsB').innerHTML = 'Unique Words: ' + matchArray.length
-        document.getElementById('wordsC').innerHTML = 'Selfsim Words: ' + (ArrayA.length) - matchArray.length
+        document.getElementById('wordsC').innerHTML = 'Selfsim Words: ' + (ArrayA.length - matchArray.length)
     }else{
         document.getElementById('wordsA').innerHTML = 'Total Words: ' + (ArrayA.length + ArrayB.length)
         document.getElementById('wordsB').innerHTML = 'Unique Words: ' + matchArray.length
-        document.getElementById('wordsC').innerHTML = 'Selfsim Words: ' + (ArrayA.length + ArrayB.length) - matchArray.length    
+        document.getElementById('wordsC').innerHTML = 'Selfsim Words: ' + ((ArrayA.length + ArrayB.length) - matchArray.length)
     }     
 }
