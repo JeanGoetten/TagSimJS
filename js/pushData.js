26dffde4e3ccdccd0a932caad0eab41e33556377
Selbstdenker <34884262+Selbstdenker@users.noreply.github.com>
2019-05-07 14:05:01 -0300

    simplify presentation

--- js/pushData.js
+++ js/pushData.js
@@ -1,9 +1,9 @@
-var pushData = (textareaID1, textareaID2, tableID, btnID) => { 
+var pushData = (textareaID1, textareaID2, tableID) => { 
     var inputText = document.getElementById(textareaID1).value;
     var inputText2 = document.getElementById(textareaID2).value;
     nullNot(inputText); 
     nullNot(inputText2); 
     var myArray = strFormat(inputText); 
     var myArrayMirror = strFormat(inputText2); 
-    selfSim(myArray, myArrayMirror, tableID, btnID); 
+    selfSim(myArray, myArrayMirror, tableID); 
 }
--- js/selfSim.js
+++ js/selfSim.js
@@ -1,25 +1,27 @@
-var selfSim = (arrayA, arrayB, tableID, btnID) => {
-    var Match = []
+var selfSim = (arrayA, arrayB, tableID) => {
+    let table = "<table cellpading='0' cellspacing='0'>"
     for(let index = 0; index < arrayA.length; index++) {
         const elementA = arrayA[index]
-        var table = document.getElementById(tableID)
-        var row = table.insertRow(index)
+        table += '<tr>'
         let i = 0
         while(i < arrayB.length) {
             const elementB = arrayB[i]
             
             if(elementA == elementB){ 
-                var cell = row.insertCell(i)
-                cell.innerHTML = "▓▓"
-                Match.push(elementA)
-
-                count(arrayA, arrayB, Match, btnID)
+                table += "<td style='background-color: #ccc' width='5px' heigth='5px' align='center' font-size='2pt'>"
+                table += ''
+                table += '</td>'
             }
             else{
-                var cell = row.insertCell(i)
-                cell.innerHTML = "░░"
+                table += "<td style='background-color: #555' width='5px' heigth='5px' align='center' font-size='2pt'>"
+                table += ''
+                table += '</td>'
             } 
             i++
         }
+        table += '</tr>'
     }
+    table += '</table>'
+
+    document.getElementById(tableID).innerHTML = table
 }
