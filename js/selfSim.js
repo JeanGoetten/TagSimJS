713628341c4de2b038a3e70b8395879f9004e97d
Selbstdenker <34884262+Selbstdenker@users.noreply.github.com>
2019-05-07 13:46:41 -0300

    Add files via upload

--- js/selfSim.js
+++ js/selfSim.js
@@ -1,25 +1,31 @@
 var selfSim = (arrayA, arrayB, tableID, btnID) => {
     var Match = []
+    let table = "<table cellpading='0' cellspacing='0'>"
     for(let index = 0; index < arrayA.length; index++) {
         const elementA = arrayA[index]
-        var table = document.getElementById(tableID)
-        var row = table.insertRow(index)
+        table += '<tr>'
         let i = 0
         while(i < arrayB.length) {
             const elementB = arrayB[i]
             
             if(elementA == elementB){ 
-                var cell = row.insertCell(i)
-                cell.innerHTML = "▓▓"
+                table += "<td style='background-color: #ccc' width='5px' heigth='5px' align='center' font-size='2pt'>"
+                table += ''
+                table += '</td>'
+        
                 Match.push(elementA)
-
                 count(arrayA, arrayB, Match, btnID)
             }
             else{
-                var cell = row.insertCell(i)
-                cell.innerHTML = "░░"
+                table += "<td style='background-color: #555' width='5px' heigth='5px' align='center' font-size='2pt'>"
+                table += ''
+                table += '</td>'
             } 
             i++
         }
+        table += '</tr>'
     }
+    table += '</table>'
+
+    document.getElementById(tableID).innerHTML = table
 }
